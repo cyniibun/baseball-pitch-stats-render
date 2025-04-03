7fff2094e8064f072c0bc3149244530c30487b37
itsCyniical <205866772+cyniibun@users.noreply.github.com>
2025-04-03 13:20:22 -0400

    beta 0.5.3

--- utils/style_helpers.py
+++ utils/style_helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.io.formats.style import Styler
 from .style_utils import (
     get_batter_red_green_shade,
     get_batter_blue_red_shade,
@@ -16,21 +17,21 @@ def sanitize_numeric_columns(df, columns):
             df[col] = pd.to_numeric(df[col], errors="coerce").fillna(0.000).round(3)
     return df
 
-def style_pitcher_table(df: pd.DataFrame) -> pd.io.formats.style.Styler:
+def style_pitcher_table(df: pd.DataFrame) -> Styler:
     """
     Apply styles to the pitcher table including color shading for specific metrics.
     """
     return df.style.applymap(get_pitcher_blue_red_shade, subset=["BA", "SLG", "wOBA"]) \
                    .applymap(lambda x: get_pitcher_red_green_shade(x, high_is_good=True), subset=["K%", "Whiff%", "PutAway%"])
 
-def style_batter_table(df: pd.DataFrame) -> pd.io.formats.style.Styler:
+def style_batter_table(df: pd.DataFrame) -> Styler:
     """
     Apply styles to the batter table including color shading for specific metrics.
     """
     return df.style.applymap(lambda x: get_batter_red_green_shade(x, high_is_bad=True), subset=["K%", "Whiff%", "PutAway%"]) \
                    .applymap(get_batter_blue_red_shade, subset=["BA", "SLG", "wOBA"])
 
-def style_delta_table(df: pd.DataFrame) -> pd.io.formats.style.Styler:
+def style_delta_table(df: pd.DataFrame) -> Styler:
     """
     Apply styles to the delta table for pitch type comparison between batter and pitcher.
     """
